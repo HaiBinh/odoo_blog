2486f2adb78dc8b320194f76b62f92e6fbd0b84e
hb <binhdh@izisolution.vn>
2021-08-27 22:08:28 +0700

    create addons_custom module

---
+++ addons_custom/website_blog_custom/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Blogs Custom',
+    'category': 'Website/Website',
+    'sequence': 140,
+    'summary': 'Publish blog posts, announces, news',
+    'version': '1.0',
+    'description': "",
+    'depends': ['website_blog'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+}
